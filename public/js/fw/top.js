ac4994feffd8a78b37226264c7514489edc1d090
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-12-02 13:21:42 +0900

    一旦コミット

--- public/js/fw/top.js
+++ public/js/fw/top.js
@@ -2,6 +2,7 @@ function clickReadNowButton() {
     var top_logo_a = $('.top_logo_a');
     var top_read_now_a = $('.top_read_now_a');
     var top_fixed = $('.top_fixed');
+    var top_fixed_div = $('.top_fixed_div');
     var menu = $('.menu-content_top');
     top_logo_a.css('opacity', '0');
     top_logo_a.css('z-index', '0');
@@ -13,6 +14,6 @@ function clickReadNowButton() {
     top_fixed.css('z-index', '0');
     top_fixed.css('pointer-events', 'none');
     menu.css('opacity', '1');
-    menu.css('z-index', '10');
+    menu.css('z-index', '1001');
     menu.css('pointer-events', 'auto');
 }
